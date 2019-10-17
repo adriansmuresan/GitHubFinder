9ffc6b9d592cbcf8acdfba0ed09c47d77cd60f90
Adrian S MUresan <adriansmuresan@gmail.com>
2019-10-17 16:57:06 -0500

    Adds fetch for the repos data

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class Github {
   constructor() {
     this.client_id = 'ec207fff6f4d75bd5556';
     this.client_secret = 'c572546289ee0c48d983eecb0af103208a5612a8';
+    this.repos_count = 5;
+    this.repos_sort = 'craeted: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}$sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile: profile
+      profile: profile,
+      repos: repos
     }
   }
 }
